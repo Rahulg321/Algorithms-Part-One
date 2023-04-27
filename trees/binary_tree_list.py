ee6519b52f34a046a260b1aa2fa55dd078df8793
Rahul Gupta <rg5353070@gmail.com>
2023-04-27 19:09:48 +0530

    made some changes

--- trees/binary_tree_list.py
+++ trees/binary_tree_list.py
@@ -1,8 +1,9 @@
-#in this file we will implement a binary tree from Python List
-class BinaryTree:
-    def __init__(self, size = 5):
-        self.custom_list = [None] * size
-        self.last_used_index = 0
-        self.max_size = size
+# in this file we will implement a binary tree from Python List
 
-BT = BinaryTree(8)
+class BinaryTreeNode:
+    def __init__(self):
+        self.name = "rahul"
+        self.work = "python_programming"
+
+    def __str__(self):
+        return self.name
